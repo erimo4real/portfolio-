28aa322d13b04ec4441d947bc4932a483ca1920f
eromosele <eromoxlx@gmail.com>
2026-03-14 01:04:21 +0100

    Add Mobile to MongoDB schema

--- backend/src/modules/skills/model.js
+++ backend/src/modules/skills/model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const SkillSchema = new mongoose.Schema(
   {
     name: String,
-    category: { type: String, enum: ["Frontend", "Backend", "DevOps", "Tooling"], index: true },
+    category: { type: String, enum: ["Frontend", "Backend", "DevOps", "Tooling", "Mobile"], index: true },
     order: { type: Number, default: 0 },
     published: { type: Boolean, default: true }
   },
